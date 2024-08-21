ae9c3f53f604ecd79ef1aefd22eee855050af8b3
mnelshazly <mnelshazly7@gmail.com>
2024-08-21 23:14:03 +0300

    add sign up logic

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
 
+  registerForm: FormGroup = new FormGroup({
+
+    name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+
+    email: new FormControl(null, [Validators.required, Validators.email]),
+
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^\w{6,}$/)]),
+
+    rePassword: new FormControl(null),
+
+    phone: new FormControl(null, [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)]),
+  }, this.confirmPassword);
+
+  confirmPassword(g:AbstractControl) {
+
+    if(g.get('password')?.value === g.get('rePassword')?.value) {
+
+      return null;
+
+    } else {
+
+      return {mismatch:true}
+
+    }
+  }
+
+  sendData ():void {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm)
+    }
+  }
 }
